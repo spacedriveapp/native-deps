aef9ea001e22a8ec8c48257b4b6703b221f02216
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2023-12-02 01:09:16 -0300

    Fix new libplacebo failing to compile resource files on Windows

--- stages/50-vulkan/60-placebo.sh
+++ stages/50-vulkan/60-placebo.sh
@@ -17,6 +17,8 @@ curl_tar 'https://github.com/pallets/jinja/archive/refs/tags/3.1.2.tar.gz' place
 curl_tar 'https://github.com/pallets/markupsafe/archive/refs/tags/2.1.3.tar.gz' placebo/3rdparty/markupsafe 1
 curl_tar 'https://github.com/fastfloat/fast_float/archive/refs/tags/v5.3.0.tar.gz' placebo/3rdparty/fast_float 1
 
+sed -i "s|windows.compile_resources(libplacebo_rc, depends: version_h,|windows.compile_resources(libplacebo_rc, depends: version_h, args: '/c65001',|" placebo/src/meson.build
+
 # Remove some superfluous files
 rm -rf placebo/{.*,docs,demos}
 rm -rf placebo/3rdparty/jinja/{.*,artwork,docs,examples,requirements,scripts,tests}
