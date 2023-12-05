6b63f1049067018291731ce4d293db5a9506376d
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2023-12-05 05:46:33 -0300

    Fix model download link

--- stages/99-yolov8.sh
+++ stages/99-yolov8.sh
@@ -3,5 +3,5 @@
 echo "Download yolov8 model..."
 
 mkdir -p "$OUT"/{models,licenses}
-curl -o "${OUT}/models/yolov8s.onnx" "https://github.com/spacedriveapp/native-deps/releases/download/yolo-2023-12-04/yolov8s.onnx"
+curl -o "${OUT}/models/yolov8s.onnx" "https://github.com/spacedriveapp/native-deps/releases/download/yolo-2023-12-05/yolov8s.onnx"
 curl -o "${OUT}/licenses/yolov8s.LICENSE" 'https://raw.githubusercontent.com/ultralytics/assets/main/LICENSE'
