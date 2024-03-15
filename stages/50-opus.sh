a799b8b3ffa0510dd7db21fe2282ffaae9f51d6e
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2024-03-15 20:52:37 -0300

    Download release src instead of tag directly from GH

--- stages/50-opus.sh
+++ stages/50-opus.sh
@@ -6,16 +6,10 @@ mkdir -p opus
 # renovate: datasource=github-tags depName=xiph/opus versioning=semver-coerced
 _tag='1.5.1'
 
-curl_tar "https://github.com/xiph/opus/archive/refs/tags/v${_tag}.tar.gz" opus 1
+curl_tar "https://ftp.osuosl.org/pub/xiph/releases/opus/opus-${_tag}.tar.gz" opus 1
 
 # Remove unused components
 rm -rf opus/{.github,CMakeLists.txt,config.sub,aclocal.m4,config.guess,cmake,doc,Makefile.in,tests,ltmain.sh,m4,configure}
-
-# Update version
-cat > "opus/package_version" <<-EOF
-	AUTO_UPDATE=no
-	PACKAGE_VERSION="$_tag"
-EOF
 
 # Backup source
 bak_src 'opus'
