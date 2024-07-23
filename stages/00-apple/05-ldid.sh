abb4aa30259a04eff9203074406c5f57dbae4a02
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2024-07-23 19:38:52 -0300

    Update vpx to include arm64 fix  - Add ldid apple tool

---
+++ stages/00-apple/05-ldid.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+case "$TARGET" in
+  x86_64-darwin*)
+    _target='x86_64-apple-darwin19'
+    ;;
+  aarch64-darwin*)
+    _target='arm64-apple-darwin20'
+    ;;
+  *)
+    exit 0
+    ;;
+esac
+
+echo "APPLE_TARGET=$_target" >>/etc/environment
+
+export CC="clang-17"
+export CXX="clang++-17"
+export CFLAGS="-I${CCTOOLS}/include"
+export LDFLAGS="-L${CCTOOLS}/lib"
+export APPLE_TARGET='__BYPASS__'
+
+cd /srv
+
+echo "Download ldid ..."
+
+mkdir -p "ldid"
+
+# renovate: depName=git@github.com:HeavenVolkoff/ldid.git
+_commit='483595e'
+
+curl_tar "https://github.com/HeavenVolkoff/ldid/archive/${_commit}.tar.gz" 'ldid' 1
+
+# renovate: datasource=github-releases depName=libimobiledevice/libplist
+_tag='2.6.0'
+
+curl_tar "https://github.com/strukturag/libde265/archive/refs/tags/v${_tag}.tar.gz" 'ldid/libplist' 1
+
+cd ldid/libplist
+
+./autogen.sh
+
+./configure \
+  --prefix="$CCTOOLS" \
+  --target="$_target" \
+  --with-pic \
+  --enable-static \
+  --without-tests \
+  --without-cython \
+  --disable-debug \
+  --disable-shared
+
+make -j"$(nproc)"
+
+cd ..
+
+make -j"$(nproc)"
+
+cp ldid "${CCTOOLS}/bin/"
+
+rm -r /srv/ldid
