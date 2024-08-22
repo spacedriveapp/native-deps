e5b3f555db9b3ad3d895c24d6c652864619ddba8
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2024-08-22 01:37:26 -0300

    Attempt to fix arm64 iossim

--- stages/10-compiler-rt.sh
+++ stages/10-compiler-rt.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env -S bash -euo pipefail
 
 case "$TARGET" in
-  *darwin*) ;;
-  *)
-    export UNSUPPORTED=1
-    exit 1
-    ;;
+*darwin*) ;;
+*)
+  export UNSUPPORTED=1
+  exit 1
+  ;;
 esac
 
 # LLVM install path
@@ -59,23 +59,57 @@ cmake_config=(
 )
 
 if [ "$_arch" == 'aarch64' ]; then
-  cmake_config+=(
-    -DDARWIN_osx_ARCHS="arm64"
-    -DDARWIN_osx_BUILTIN_ARCHS="arm64"
-    -DDARWIN_ios_ARCHS="arm64"
-    -DDARWIN_ios_BUILTIN_ARCHS="arm64"
-    -DDARWIN_iossim_ARCHS="arm64"
-    -DDARWIN_iossim_BUILTIN_ARCHS="arm64"
-  )
+  if [ "$OS_IPHONE" -ge 0 ]; then
+    cmake_config+=(
+      -DDARWIN_osx_ARCHS="arm64"
+      -DDARWIN_osx_BUILTIN_ARCHS="arm64"
+      -DDARWIN_ios_ARCHS=""
+      -DDARWIN_ios_BUILTIN_ARCHS=""
+      -DDARWIN_iossim_ARCHS=""
+      -DDARWIN_iossim_BUILTIN_ARCHS=""
+    )
+  elif [ "$OS_IPHONE" -ge 1 ]; then
+    cmake_config+=(
+      -DDARWIN_osx_ARCHS=""
+      -DDARWIN_osx_BUILTIN_ARCHS=""
+      -DDARWIN_ios_ARCHS="arm64"
+      -DDARWIN_ios_BUILTIN_ARCHS="arm64"
+      -DDARWIN_iossim_ARCHS=""
+      -DDARWIN_iossim_BUILTIN_ARCHS=""
+    )
+  elif [ "$OS_IPHONE" -ge 2 ]; then
+    cmake_config+=(
+      -DDARWIN_osx_ARCHS=""
+      -DDARWIN_osx_BUILTIN_ARCHS=""
+      -DDARWIN_ios_ARCHS=""
+      -DDARWIN_ios_BUILTIN_ARCHS=""
+      -DDARWIN_iossim_ARCHS="arm64"
+      -DDARWIN_iossim_BUILTIN_ARCHS="arm64"
+    )
+  fi
 else
-  cmake_config+=(
-    -DDARWIN_osx_ARCHS="$_arch"
-    -DDARWIN_osx_BUILTIN_ARCHS="$_arch"
-    -DDARWIN_ios_ARCHS="$_arch"
-    -DDARWIN_ios_BUILTIN_ARCHS="$_arch"
-    -DDARWIN_iossim_ARCHS="$_arch"
-    -DDARWIN_iossim_BUILTIN_ARCHS="$_arch"
-  )
+  if [ "$OS_IPHONE" -ge 0 ]; then
+    cmake_config+=(
+      -DDARWIN_osx_ARCHS="$_arch"
+      -DDARWIN_osx_BUILTIN_ARCHS="$_arch"
+      -DDARWIN_ios_ARCHS=""
+      -DDARWIN_ios_BUILTIN_ARCHS=""
+      -DDARWIN_iossim_ARCHS=""
+      -DDARWIN_iossim_BUILTIN_ARCHS=""
+    )
+  elif [ "$OS_IPHONE" -ge 1 ]; then
+    echo "iOS don't support $_arch" >&2
+    exit 1
+  elif [ "$OS_IPHONE" -ge 2 ]; then
+    cmake_config+=(
+      -DDARWIN_osx_ARCHS=""
+      -DDARWIN_osx_BUILTIN_ARCHS=""
+      -DDARWIN_ios_ARCHS=""
+      -DDARWIN_ios_BUILTIN_ARCHS=""
+      -DDARWIN_iossim_ARCHS="$_arch"
+      -DDARWIN_iossim_BUILTIN_ARCHS="$_arch"
+    )
+  fi
 fi
 
 cmake "${cmake_config[@]}" ..
